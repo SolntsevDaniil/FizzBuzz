f46797a92e846f130f0dec0861b2830536457fc5
I and O <31993463+SolntsevDaniil@users.noreply.github.com>
2018-04-22 20:49:42 +0300

    init

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 'use strict';
-function FizzBuzz() {
-	for(let i = 0; i < 100;)  {
-		console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
-	};
+for(let i = 0; i < 100;)  {
+	console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
 };
